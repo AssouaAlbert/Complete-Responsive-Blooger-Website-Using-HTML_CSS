bc6f3d2d5ae3c1cd3173c4dc6b92e6a38f0e6beb
Eloumbat Assoua Albert <assouaalbert@gmail.com>
2020-05-24 02:23:48 +0300

    Great Job

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,19 @@
+// Define the break points
+const responsive = {
+    0: {
+        items: 1
+    },
+    600: {
+        items: 2
+    },
+    750: {
+        items: 2
+    }
+}
+
 $(document).ready(function(){
+    //AOS instance
+    AOS.init();
     $nav = $('.nav');
     $toggleCollapse =$('.toggle-collapse');
 
@@ -12,5 +27,16 @@ $(document).ready(function(){
         autoplay: true,
         autoplayTimeout: 5000,
         // nav: [$('.slider-navigation-left'), $('.slider-navigation-right')]
+        responsive: responsive
+    });
+    // Clock to scroll up
+    $('.move-up span').click(function(){
+        $('html, body').animate({
+            scrollTop:0
+        },500);
+    });
+    $('.post-content').attr({
+        "data-aos":"fade-up",
+        "data-aos-delay": "200"
     });
 });
