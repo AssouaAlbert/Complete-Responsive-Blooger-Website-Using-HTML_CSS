33ca321675398edc97e1c1121348513942d9490a
Eloumbat Assoua Albert <assouaalbert@gmail.com>
2020-05-24 00:33:11 +0300

    Almost there

--- scripts/app.js
+++ scripts/app.js
@@ -6,4 +6,11 @@ $(document).ready(function(){
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     });
+    //owl.carousel class
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        // nav: [$('.slider-navigation-left'), $('.slider-navigation-right')]
+    });
 });
